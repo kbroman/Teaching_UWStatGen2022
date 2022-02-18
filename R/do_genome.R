cb16946d78479f6a47006a7804278bd2c0d269e0
Karl Broman <kbroman@gmail.com>
2022-02-18 12:56:09 -0600

    Small adjustment to DO genome figure

--- R/do_genome.R
+++ R/do_genome.R
@@ -9,5 +9,5 @@ pdf(here("Figs/do_genome.pdf"), width=9.75, height=5, pointsize=16)
 par(mar=c(3.1,4.1,0.6,0.6))
 plot_onegeno(ph, pmap, mgp.x=c(1.8,0.3,0),
              ylab="Position (Mbp)")
-legend(10.5, 163, names(CCcolors), pt.bg=CCcolors, pch=22, bg="gray90", ncol=4)
+legend(9.5, 163, names(CCcolors), pt.bg=CCcolors, pch=22, bg="gray90", ncol=4)
 dev.off()
